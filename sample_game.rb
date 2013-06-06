1ecdf9890575abdb519b98b9668ed95483fb9b56
Eric Sare <eric.sare@akqa.com>
2013-06-06 16:06:51 -0700

    added sample_game.rb

---
+++ sample_game.rb
@@ -0,0 +1,12 @@
+require_relative 'game'
+require_relative 'plateau'
+require_relative 'rover'
+
+game = Game.new()
+game.start()
+
+game.rovers.each_with_index do |rover, index|
+  puts "rover#{index} location: #{rover.status}"
+end
+
+

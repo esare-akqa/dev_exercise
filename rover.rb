e5d6a39ad0c9f8716a494415e2e460f07d2f1068
Eric Sare <eric.sare@akqa.com>
2013-06-05 17:53:17 -0700

    added initialize on rover.rb and added instance test for rover_spec.rb

--- rover.rb
+++ rover.rb
@@ -1,3 +1,14 @@
 class Rover
-  
+  attr_accessor :coord, :deg
+
+  def initialize(input)
+    @coord = Array.new(2)
+    @heading = {'N' =>  90, 'E' =>  0, 'S' =>  270, 'W' =>  180}
+    input = input.split(' ')
+    coord[0], coord[1], @deg = input[0], input[1], set_deg(input[2])
+  end
+
+  def set_deg(input)
+    @deg = @heading[input] if @heading.has_key? input
+  end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Rover do
-  # before :each do
-  #   @rover = Rover.new()
-  # end
+  before :each do
+    @rover = Rover.new('1 2 N')
+  end
+
+  describe '#new' do
+    it 'takes a string with 2 digits and a letter, and returns a Rover object' do
+      @rover.should be_an_instance_of(Rover)
+    end
+  end
 end

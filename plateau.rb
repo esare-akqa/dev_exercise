9985a38b6a9350c6862018a7f0cccd819fc565cb
Eric Sare <eric.sare@akqa.com>
2013-06-06 12:33:47 -0700

    added plateau.rb, plateau_spec.rb and test for initalize method

---
+++ plateau.rb
@@ -0,0 +1,7 @@
+class Plateau
+  attr_reader :width, :length
+  def initialize(width=10, length=10)
+    @width = width
+    @length = length
+  end
+end
---
+++ spec/plateau_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Plateau do
+  before :each do
+    @plateau = Plateau.new(5, 5)
+    @plateau2 = Plateau.new()
+  end
+
+  describe '#new' do
+    it 'is an instance of Plateau' do
+      @plateau.should be_an_instance_of(Plateau)
+    end
+  end
+
+  describe '#width' do
+    it 'should not be nil' do
+      @plateau.width.should_not equal nil
+    end
+    it 'is a Fixnum' do
+      @plateau.width.should be_an_instance_of(Fixnum)
+    end
+  end
+
+  describe '#length' do
+    it 'is a Fixnum' do
+      @plateau.length.should be_an_instance_of(Fixnum)
+    end
+    it 'should not be nil' do
+      @plateau.length.should_not equal nil
+    end
+  end
+end
